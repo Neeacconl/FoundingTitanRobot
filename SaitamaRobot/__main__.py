a662060aec465ace43ce8e9c14e946739cd26ed2
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-07-30 17:20:38 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -72,6 +72,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 👋 Hello Waste (〃＾▽＾〃)o 🇮🇳!.
+
 DC Checker is a bot that allows you to easily and quickly check the DC of users with multiple commands and functions.
 ❓ How does it work?.
 Type /help or press the » 🛠 Commands button below to view the entire command list.
