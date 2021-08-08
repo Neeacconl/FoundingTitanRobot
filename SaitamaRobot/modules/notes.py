c7850dd8928e5bea47f0d63b488c392c407fa735
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-08-08 10:43:34 +0530

    Update notes.py

--- SaitamaRobot/modules/notes.py
+++ SaitamaRobot/modules/notes.py
@@ -542,7 +542,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "Saver"
+__mod_name__ = "⤵️ Sichern"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
