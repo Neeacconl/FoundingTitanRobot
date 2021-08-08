34829e45810fa59a48b189b9c3959d9a19693efc
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-08-08 10:33:49 +0530

    Update bans.py

--- SaitamaRobot/modules/bans.py
+++ SaitamaRobot/modules/bans.py
@@ -423,7 +423,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(PUNCHME_HANDLER)
 
-__mod_name__ = "Kicks❓"
+__mod_name__ = "🚫 Kick"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
