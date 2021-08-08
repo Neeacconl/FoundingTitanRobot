3f697ad0387d65ff853035b9f00f991aacc6201f
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-08-08 10:22:31 +0530

    Update cleaner.py

--- SaitamaRobot/modules/cleaner.py
+++ SaitamaRobot/modules/cleaner.py
@@ -254,7 +254,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "Bluetext 👾"
+__mod_name__ = "❓ Unbekannte Befehle"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
