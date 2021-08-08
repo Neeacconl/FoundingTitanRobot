eb52ffd1ae491fdc5f1127e33e5e09ccbd6f1dda
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-08-08 10:32:54 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -562,7 +562,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Hp System"
+__mod_name__ = "👤 Hp System"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
