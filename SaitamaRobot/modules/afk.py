3c94ed2732c0aa20824c4ecfe8b443752cf6a79c
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-08-08 17:17:24 +0530

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -177,7 +177,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "Status 🎗️"
+__mod_name__ = "〽️ Status"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
